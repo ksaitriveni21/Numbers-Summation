18fc8e9c2813ac5723b76523721ee26f8dfc4670
Sai Triveni Kottapalli <86417664+ksaitriveni21@users.noreply.github.com>
2025-03-31 01:52:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -32,4 +32,5 @@ def index():
     return render_template("index.html", prediction=prediction, model=selected_model)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
